ff5f1c489334c022732e0a411e73415a3d86de4f
Prin-S <98200861+Prin-S@users.noreply.github.com>
2024-12-07 15:15:51 +0700

    Cards switch positions when one is clicked.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,26 @@
 import { useState, useEffect } from 'react';
+import '../styles/styles.css';
 
 let didInit = false;
+
+function Box({ details, onBoxClick }){
+  return (
+    <div id={details.number} className="box" onClick={onBoxClick}>
+      <div><img id={details.number} className="img" src={details.pic} alt={details.name} /></div>
+      <p id={details.number}>{details.name}</p>
+    </div>
+  );
+}
+
+function Display({ data, onBoxClick }) {
+  return (
+    <div className="container">
+      {data.map(item => {
+        return <Box key={item.number} details={item} onBoxClick={onBoxClick} />
+      })}
+    </div>
+  );
+}
 
 function App() {
   const [pokeData, setPokeData] = useState([]);
@@ -30,6 +50,7 @@ function App() {
           .then(response => {
             element.name = response.name[0].toUpperCase() + response.name.slice(1);
             element.pic = response.sprites.other['official-artwork'].front_default;
+            element.clicked = false;
             count++;
 
             if (count == 12) {
@@ -40,17 +61,28 @@ function App() {
     }
   }, []);
 
-  function handleClickButton() {
-    console.log(pokeData);
+  function shuffleArray(arr) { // https://javascript.info/task/shuffle
+    for (let i = arr.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+  }
+
+  function handleBoxClick(e) {
+    console.log(e.target.id);
+    const itemToMarkClicked = pokeData.find(item => item.number == e.target.id);
+    itemToMarkClicked.clicked = true;
+    
+    const newPokeData = [ ...pokeData.filter(item => item.number != e.target.id), itemToMarkClicked ];
+    shuffleArray(newPokeData);
+    setPokeData(newPokeData);
   }
 
   return (
     <>
-      <p>
-        <button className="button" onClick={handleClickButton}>Check</button>
-      </p>
+      <Display data={pokeData} onBoxClick={handleBoxClick} />
     </>
-  )
+  );
 }
 
 export { App };
